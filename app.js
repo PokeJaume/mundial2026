b5ec1e8b7d3c0a7f86852af945eaa93a069183e0
Jimito <pokejaumes@gmail.com>
2026-06-07 22:43:08 +0200

    Modificar variables

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 const DATA_SRC = 'https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026';
 // EDITA ESTAS 3 COSAS POR FAVOR
 // POR FAVOR
-const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDbPhOej3DnN_bdvrCQ5R0T6HZg6bBaxKdH17J_Pc3oGOkKkd9V83BUDYlBSCevOrqYK2XQuA7ZMCx/pub?gid=1633860364&single=true&output=csv'
-const FORM_ID = '1FAIpQLSdiF0qsK65DcaadNKRzDbue8xtkzAIIev-7yqUqAH3srhEAQg';
-const ENTRY_ID = 'entry.496944209';
+const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTXlfmHAN6GRiB4ng4Td8Vng6QEhYNidWTwOLvYK69MuqRGxD6lBWoNmby-m52o28Zwq7nHJ4G5x5Vt/pub?output=csv'
+const FORM_ID = '1FAIpQLSfDBwn45EjbptP5GuhC3ynkkteMOe9DwS87X_Uo2CrfiBiziw';
+const ENTRY_ID = 'entry.533779599';
 // REPITO, POR FAVOR
 
 // Puedes cambiar los valores por tus propias puntuaciones si quieres
